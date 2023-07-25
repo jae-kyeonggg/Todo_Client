af42edf0a0a33da2392efcf87177fbb2d4a84669
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-07-25 14:48:26 +0900

    adopt Github Login

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Grid, Typography, TextField, Button } from "@mui/material";
-import { signin } from "./service/ApiService";
+import { signin, socialLogin } from "./service/ApiService";
 import { Link } from "react-router-dom";
 
 function Login() {
@@ -10,6 +10,10 @@ function Login() {
     const username = data.get("username");
     const password = data.get("password");
     signin({ username: username, password: password });
+  };
+
+  const handleSocialLogin = (provider) => {
+    socialLogin(provider);
   };
 
   return (
@@ -50,6 +54,16 @@ function Login() {
           <Grid item xs={12}>
             <Button type="submit" fullWidth variant="contained" color="primary">
               로그인
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              onClick={() => handleSocialLogin("github")}
+              fullWidth
+              variant="contained"
+              style={{ backgroundColor: "#000" }}
+            >
+              깃허브로 로그인하기
             </Button>
           </Grid>
           <Grid>
